dde14766a87756165e76c07e9f3cc8e3167bb8d3
Subpar77 <jjacobs6.jj@gmail.com>
2026-07-23 17:03:18 -0400

    Removed comments (testing github)

--- src/main/java/net/jason/mccoursemod/MCCourseMod.java
+++ src/main/java/net/jason/mccoursemod/MCCourseMod.java
@@ -41,11 +41,8 @@ public class MCCourseMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
         // Register the item to a creative tab
