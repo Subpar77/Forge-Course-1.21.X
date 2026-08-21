af332590ffabbd6e0eb8ed0786f4b9ad9e8b7471
Subpar77 <jjacobs6.jj@gmail.com>
2026-08-21 12:55:12 -0400

    Added taming for Rhino

--- src/main/java/net/jason/mccourse/entity/client/RhinoModel.java
+++ src/main/java/net/jason/mccourse/entity/client/RhinoModel.java
@@ -139,6 +139,7 @@ public class RhinoModel<T extends RhinoEntity> extends HierarchicalModel<T> {
         this.animateWalk(ModAnimationDefinitions.RHINO_WALK, limbSwing, limbSwingAmount, 2f, 2.5f);
         this.animate(entity.idleAnimationState, ModAnimationDefinitions.RHINO_IDLE, ageInTicks, 1f);
         this.animate(entity.attackAnimationState, ModAnimationDefinitions.RHINO_ATTACK, ageInTicks, 1f);
+        this.animate(entity.sitAnimationState, ModAnimationDefinitions.RHINO_SIT, ageInTicks, 1f);
 
 
 	}
